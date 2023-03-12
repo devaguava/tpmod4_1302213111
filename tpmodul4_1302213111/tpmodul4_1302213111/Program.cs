ae51e8a6c8b72d745b206dd6ea575f23a598ee95
devaguava <117663776+devaguava@users.noreply.github.com>
2023-03-12 18:06:59 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213111/tpmodul4_1302213111/Program.cs
+++ tpmodul4_1302213111/tpmodul4_1302213111/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Halo Dunia!");
+
+public class KodePos
+{
+    public enum Kelurahan{ Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja }
+
+    public static int getKodePos(Kelurahan kelurahan)
+    {
+        int[] kode_pos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kode_pos[(int)kelurahan];
+    }
+
+
+
+    public static void Main()
+    {
+        KodePos kode_pos = new KodePos();
+        int getKodePos = KodePos.getKodePos(KodePos.Kelurahan.Jatisari);
+        Console.WriteLine(getKodePos); //kode pos seharusnya : 40286
+        Console.WriteLine();
+    }
+}
+
